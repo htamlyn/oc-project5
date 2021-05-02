4cd4240de8e6d80f254853e4b0595ef5a69625b8
htamlyn <helentamlyn@gmail.com>
2021-05-02 17:09:07 +0100

    Split the api functions

--- docs/product.js
+++ docs/product.js
@@ -11,25 +11,30 @@ const urlParams = new URLSearchParams(queryString);
 const id = urlParams.get('id')
 console.log(id)
 
+let data = {}
+
 const getProduct = async () => {
     try{
         const res = await fetch(`http://localhost:3000/api/teddies/${id}`);
-        const data = await res.json();
-        console.log(data);
-        productName.innerText = data.name;
-        productPrice.innerText = `$ ${data.price / 100}`;
-        productImage.src = data.imageUrl;
-        productDescription.innerText = data.description;
-        productId.innerText = data._id
-        const colors = data.colors;
-        for (color of colors) {
-            let newOption = document.createElement('option');
-            newOption.innerText = color;
-            newOption.value = color;
-            productColour.appendChild(newOption);
-        }
+        data = await res.json();
+        displayProduct();
     } catch(e){
         console.log('error')
+    }
+}
+
+function displayProduct() {
+    productName.innerText = data.name;
+    productPrice.innerText = `$ ${data.price / 100}`;
+    productImage.src = data.imageUrl;
+    productDescription.innerText = data.description;
+    productId.innerText = data._id
+    const colors = data.colors;
+    for (color of colors) {
+        let newOption = document.createElement('option');
+        newOption.innerText = color;
+        newOption.value = color;
+        productColour.appendChild(newOption);
     }
 }
 
--- docs/products.js
+++ docs/products.js
@@ -3,44 +3,51 @@ const productPrices = [document.querySelector('#product1Price'), document.queryS
 const productImages = [document.querySelector('#product1Image'), document.querySelector('#product2Image'), document.querySelector('#product3Image'), document.querySelector('#product4Image'), document.querySelector('#product5Image')];
 const productIds = [document.querySelector('#product1Id'), document.querySelector('#product2Id'), document.querySelector('#product3Id'), document.querySelector('#product4Id'), document.querySelector('#product5Id')];
 
-const populateProduct = async () => {
+
+let names = [];
+let prices = [];
+let images = [];
+let ids = [];
+const fetchProduct = async () => {
     try {
         const res = await fetch("http://localhost:3000/api/teddies/");
         const data = await res.json();
-        let names = [];
-        let prices = [];
-        let images = [];
-        let ids = [];
         for (item of data) {
             names.push(item.name);
             prices.push(item.price / 100);
             images.push(item.imageUrl);
             ids.push(item._id);
         }
-        productNames.forEach((product, index) => {
-            let name = names[index];
-            product.innerText = name;
-        })
-        productPrices.forEach((product, index) => {
-            let price = prices[index];
-            product.innerText = `$ ${price}`;
-        })
-        productImages.forEach((product, index) => {
-            let image = images[index];
-            product.src = image;
-        })
-        productIds.forEach((product, index) => {
-            let id = ids[index];
-            product.value = id;
-            product.addEventListener('click', () => {
-                console.log('clicked')
-                window.location.href = `product.html?id=${product.value}`
-            })
-        })
+        populateProduct();
     } catch (e) {
         console.log('error')
-    }
+    } 
+    
 }
 
-populateProduct();
+function populateProduct() {
+    productNames.forEach((product, index) => {
+        let name = names[index];
+        product.innerText = name;
+    })
+    productPrices.forEach((product, index) => {
+        let price = prices[index];
+        product.innerText = `$ ${price}`;
+    })
+    productImages.forEach((product, index) => {
+        let image = images[index];
+        product.src = image;
+    })
+    productIds.forEach((product, index) => {
+        let id = ids[index];
+        product.value = id;
+        product.addEventListener('click', () => {
+            console.log('clicked')
+            window.location.href = `product.html?id=${product.value}`
+        })
+    })
+}
 
+fetchProduct();
+
+module.exports.fetch = fetchProduct;
